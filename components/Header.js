e308f9c9d9d1ea942a2e74d95da42d38964fa795
Jorki3 <eduardomttzt01@outlook.com>
2022-09-19 16:57:41 -0500

    Estilos en Header

---
+++ components/Header.js
@@ -0,0 +1,21 @@
+import Link from "next/link"
+import styles from "../styles/Header.module.css"
+
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            <div className="contenedor">
+                <div className=""></div>
+
+                <nav className={styles.navegacion}>
+                    <Link href="/">Inicio</Link>
+                    <Link href="/nosotros">Nosotros</Link>
+                    <Link href="/blog">Blog</Link>
+                    <Link href="/tienda">Tienda</Link>
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+export default Header
